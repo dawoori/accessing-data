be25284dbc51992716925b6ab91abf970a6fc40c
dawoori <daul@kookmin.ac.kr>
2020-05-22 16:33:54 +0900

    H2 Console 외부 접속

--- src/main/java/com/example/accessingdata/MainController.java
+++ src/main/java/com/example/accessingdata/MainController.java
@@ -1,6 +1,7 @@
 package com.example.accessingdata;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
